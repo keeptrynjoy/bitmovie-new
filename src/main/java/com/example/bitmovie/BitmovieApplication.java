31feb7c18a14aea930fa29fd958ae4d3c3023a5a
taemin <myloopmy@gmail.com>
2022-11-04 16:02:18 +0900

    first

---
+++ src/main/java/com/example/bitmovie/BitmovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitmovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitmovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bitmovie/BitmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitmovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
