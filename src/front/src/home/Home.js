fc6e892f0508ce28d7a362208d95b4089304e1a3
greetjeehun <greet123@naver.com>
2022-11-08 13:26:58 +0900

    [Conflict] 11/08 pm 1:26 error 해결

--- src/front/src/home/Home.js
+++ src/front/src/home/Home.js
@@ -9,61 +9,62 @@ function Home(props) {
     const key ='3e56c5d518bc82f65d4d1d16806fdd37';
     const today = new Date();
     const targetDT = today.getFullYear()+(today.getMonth()).toString().padStart(2,0)+(today.getDate().toString().padStart(2,0));
-        const getMovies = async () => {
-            const json = await (
-                await fetch(
-                    `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDT}`
-                )
-            ).json();
-            setMovies(json.boxOfficeResult.dailyBoxOfficeList);;
-            setLoading(false);
-        };
-        var carousels = document.querySelectorAll('.glide');
-        useEffect(() => {
-            getMovies();
-        }, []);
-        useEffect(() => {
-            console.log(carousels.length);
-            for(var i = 0 ; i < carousels.length; i++){
-                var glide = new Glide(carousels[i], {
-                    type: 'carousel',
-                    startAt: 0,
-                    perView: 5,
-                });
-                glide.mount();
-            }
-        }, [carousels]);
-        return (
-            <div>
-                <h1> today boxoffice</h1>
-                    <div className="Home">
-                        {loading ? (
-                            <p>loading...</p>) : (
-                                    <div>
-                                        <div className="frames glide__track" data-glide-el="track">
-                                            <ul className = "frames__list glide__slides">
-                                                {movies.map((movie) => (
-                                                    <li className="frames__item glide__slide" key={movie.rank}>
-                                                        <div className="boxoffice">
-                                                            <h2> {movie.rank} </h2>
-                                                            <p> {movie.movieNm} ({movie.openDt.substr(0, 4)}) </p>
-                                                            <p> 오늘 관객수: {movie.audiCnt} </p>
-                                                            <p> 누적 관객수: {movie.audiAcc} </p>
-                                                        </div>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                        <div data-glide-el="controls">
-                                            <button data-glide-dir="<">&lt;</button>
-                                            <button data-glide-dir=">">&gt;</button>
-                                        </div>
-                                    </div>
-                                    )}
-                                </div>
-                            <h1> week boxofiice </h1>
-                            </div>
-        )
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDT}`
+            )
+        ).json();
+        setMovies(json.boxOfficeResult.dailyBoxOfficeList);;
+        setLoading(false);
+    };
+    var carousels = document.querySelectorAll('.glide');
+    useEffect(() => {
+        getMovies();
+    }, []);
+    useEffect(() => {
+        console.log(carousels.length);
+        for(var i = 0 ; i < carousels.length; i++){
+            var glide = new Glide(carousels[i], {
+                type: 'carousel',
+                startAt: 0,
+                perView: 5,
+            });
+            glide.mount();
+        }
+    }, [carousels]);
 
-    export default Home;
+    return (
+        <div>
+            <h1> today boxoffice</h1>
+            <div className="Home">
+                {loading ? (
+                    <p>loading...</p>) : (
+                    <div>
+                        <div className="frames glide__track" data-glide-el="track">
+                            <ul className = "frames__list glide__slides">
+                                {movies.map((movie) => (
+                                    <li className="frames__item glide__slide" key={movie.rank}>
+                                        <div className="boxoffice">
+                                            <h2> {movie.rank} </h2>
+                                            <p> {movie.movieNm} ({movie.openDt.substr(0, 4)}) </p>
+                                            <p> 오늘 관객수: {movie.audiCnt} </p>
+                                            <p> 누적 관객수: {movie.audiAcc} </p>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div data-glide-el="controls">
+                            <button data-glide-dir="<">&lt;</button>
+                            <button data-glide-dir=">">&gt;</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <h1> week boxofiice </h1>
+        </div>
+    )
+
 }
+export default Home;
--- src/front/src/home/LoginTrigger.js
+++ src/front/src/home/LoginTrigger.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Typography from "@mui/material/Typography";
 import {NavLink} from "react-router-dom";
 import {styled} from "@mui/material/styles";
