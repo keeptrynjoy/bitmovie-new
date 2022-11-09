339fb8990654fe42a025542523891b8aa317c464
keeptrynjoy <sungmin4218@gmail.com>
2022-11-09 16:58:46 +0900

    [Add,Feat] 1. GlobalException 기본 세팅

--- src/main/java/data/repository/PaymentRepository.java
+++ src/main/java/data/repository/PaymentRepository.java
@@ -1,7 +1,12 @@
 package data.repository;
 
+import data.domain.Payment;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface PaymentRepository {
+
+    public int selectPaymentData(Payment payment);
 }
