1bf4583bf4e5c5e4066296a83a3fa2ada4390dfa
taemin <myloopmy@gmail.com>
2022-11-09 17:01:58 +0900

    [Add] MainPage 평점구간 슬라이드 추가 영화리스트, 디테일 페이지 추가

--- src/front/src/movie/detail/MovieDetail.js
+++ src/front/src/movie/detail/MovieDetail.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
+import "./Detail.css";
 
 function MovieDetail(props) {
     const p = useParams();
+
     return (
         <div>
-            <h1>상세페이지</h1>
-            <h2>{p.movie_num}번</h2>
+           <div className={'dtall'}>
+               <div className={"dtposter"}></div>
+               <div className={"dtcontent"}>
+                   <div className={"dtname"}>
+                       영화제목 : 000<br/>
+                       예매율 : 000<br/>
+                       평점 : 000<br/>
+                       감독/출연진 소개 : 000<br/>
+                       영화 간단 줄거리 : 000<br/>
+                   </div>
+                   <button type={"button"} className={"dtbt"}>예매하기</button>
+               </div>
+               <div className={"prevideo"}>영화예고편 자리</div>
+               <div className={"story"}>영화줄거리 자리</div>
+           </div>
+
         </div>
     );
 }
