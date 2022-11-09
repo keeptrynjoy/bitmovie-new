68ed8ee14f8b26c06c82fc618bea072d8e831dd2
keeptrynjoy <sungmin4218@gmail.com>
2022-11-09 20:53:36 +0900

    [Feat] 1. front Test 페이지 추가

--- src/front/src/routers/TicketingRouter.js
+++ src/front/src/routers/TicketingRouter.js
@@ -3,12 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import Payment from "../ticketing/payment/Payment";
 import SelectSeat from "../ticketing/payment/SelectSeat";
 import Ticketing from "../ticketing/Ticketing";
+import BookingTest from "../ticketing/BookingTest";
 
 function TicketingRouter(props) {
     return (
         <div>
             <Routes>
                 <Route path={"/"} element={<Ticketing/>}/>
+                <Route path={"/test"} element={<BookingTest/>}/>
                 <Route path={"payment"} element={<Payment/>}/>
                 <Route path={"selectseat"} element={<SelectSeat/>}/>
                 <Route path={"*"} element={
