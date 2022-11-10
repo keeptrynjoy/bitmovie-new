b3f32891a84e64b850ec65764cc81c937628b468
joonmin) <90_kh@naver.com>
2022-11-10 15:02:50 +0900

    [Feat] 로그인 시 비밀번호 변경 후 지난 기간 반환 기능 추가 [Feat] 비밀번호 변경 관련 백엔드 기능 추가 [Feat] 회원 탈퇴 시 상태 변경하는 백엔드 기능 추가

--- src/main/java/data/repository/UserRepository.java
+++ src/main/java/data/repository/UserRepository.java
@@ -9,6 +9,13 @@ import java.util.Map;
 public interface UserRepository {
     public int selectLogin (Map<String, String> map); //로그인
     public String selectName (String u_id); //로그인 시 이름 가져오기
+    public int selectPwUdtDate (Map<String, String> map); //로그인 시 비밀번호 변경 후 지난 기간 불러오기
+
     public int searchId (String u_id); //회원가입 아이디 중복 체크
     public void insertUser (User user); //회원가입
+    public String selectPass (String u_id); //비밀번호 변경할 때 아이디 참조해서 기존 비밀번호 가져오기(입력한 비밀번호와 일치하는 지 확인용)
+    public void updatePass (Map<String, String> map); //비밀번호 변경
+
+    public void deleteUser (String u_id); //회원 삭제 (상태 변경)
+
 }
--- src/main/java/data/service/UserService.java
+++ src/main/java/data/service/UserService.java
@@ -5,7 +5,6 @@ import data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,24 +18,45 @@ public class UserService {
     public Map<String, Object> selectLogin (@RequestBody Map<String, String> map) {
         int yesOrNo = userRepository.selectLogin(map);
 
+        int pwUdtDate = 0;
         String u_name = "";
         if (yesOrNo == 1) {
             u_name = userRepository.selectName(map.get("u_id")); //로그인 성공하면 이름 가져오기
+            pwUdtDate = userRepository.selectPwUdtDate(map); //로그인 성공하면 비밀번호 변경 후 지난 기간 가져오기
         }
 
         Map<String, Object> sendMap = new HashMap<>();
         sendMap.put("yesOrNo", yesOrNo);
         sendMap.put("u_name", u_name);
+        sendMap.put("pwUdtDate", pwUdtDate);
 
         return sendMap;
     }
 
     //회원가입 아이디 중복 체크
-    public int searchId (@RequestParam String u_id) {
+    public int searchId (String u_id) {
         return userRepository.searchId(u_id);
     }
     //회원가입
     public void insertUser (@RequestBody User user) {
         userRepository.insertUser(user);
+    }
+    //비밀번호 변경할 때 아이디 참조해서 기존 비밀번호 가져오기(입력한 비밀번호와 일치하는 지 확인용)
+    public boolean selectPass (String u_id, String u_pass) {
+        String pass = userRepository.selectPass(u_id);
+        boolean check = false;
+        if (pass == u_pass) {
+            check = true;
+        }
+        return check;
+    }
+    //비밀번호 변경
+    public void updatePass (Map<String, String> map) {
+        userRepository.updatePass(map);
+    }
+
+    //회원 삭제(상태 변경)
+    public void deleteUser (String u_id) {
+        userRepository.deleteUser(u_id);
     }
 }
