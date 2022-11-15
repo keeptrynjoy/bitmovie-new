d855c2e369362364cc13d4dcf6a80f336bc40e87
greetjeehun <greet123@naver.com>
2022-11-15 17:00:01 +0900

    [Feat] sweetalert 추가

--- src/front/src/mypage/MyPageContents.js
+++ src/front/src/mypage/MyPageContents.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ChangeUserInfo from "./ChangeUserInfo";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import Swal from "sweetalert2";
 
 //영수증
 const booking=()=>{
@@ -51,22 +52,50 @@ const pointHistory=()=>{
 //회원 탈퇴
 const WithDrawal=(navi)=>{
     const withdrawalUrl = localStorage.url + "/user/delete?u_id=" + sessionStorage.u_id;
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
     return (
         <div style={{margin:"auto",textAlign:"center", width:"700px", height:"500px"}}>
             <button style={{fontSize:"70px"}} className={"btn btn-danger"}
                     type={"button"} variant={"outlined"} color={"error"}
             onClick={()=>{
-                axios.get(withdrawalUrl)
-                    .then((res) => {
-                        alert("탈퇴성공!");
-                        // sessionStorage.removeItem("login_status");
-                        // sessionStorage.removeItem("u_name");
-                        // sessionStorage.removeItem("u_id");
-                        // sessionStorage.removeItem("u_pk");
-                        // sessionStorage.removeItem("pwUdtDate");
-                        // navi("/");
-                        // window.location.reload();
-                    })
+                swalWithBootstrapButtons.fire({
+                    title: '탈퇴하시겠습니까?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: '확인',
+                    cancelButtonText: '취소',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        axios.get(withdrawalUrl)
+                            .then((res) => {
+                                sessionStorage.removeItem("login_status");
+                                sessionStorage.removeItem("u_name");
+                                sessionStorage.removeItem("u_id");
+                                sessionStorage.removeItem("u_pk");
+                                sessionStorage.removeItem("pwUdtDate");
+                                navi("/");
+                                window.location.reload();
+                            })
+                        swalWithBootstrapButtons.fire(
+                            '탈퇴성공!',
+                        )
+                    } else if (
+                        /* Read more about handling dismissals below */
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        swalWithBootstrapButtons.fire(
+                            '탈퇴취소',
+                        )
+                    }
+                })
             }}>
                 BITMOVIE 탈퇴
             </button>
