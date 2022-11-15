b223f51ca69804e5bf30be9619f3d5d06d8b2c9d
joonmin) <90_kh@naver.com>
2022-11-15 12:38:44 +0900

    [Refactor] User에서 쓰던 MyPage 기능들 클래스 파일들 MyPage로 새로 만들어서 옮김

--- src/main/java/data/controller/user/UserController.java
+++ src/main/java/data/controller/user/UserController.java
@@ -48,37 +48,6 @@ public class UserController {
     //비밀번호 찾기(아이디, 핸드폰 번호 넘겨서 둘 다 일치하는 레코드 있으면 1, 없으면 0 넘겨줌)
     @GetMapping("/findpass")
     public int selectFindPass (@RequestParam Map<String, String> map) {
-        System.out.println(map);
         return userService.selectFindPass(map);
-    }
-    //마이페이지 유저 정보 출력
-    @GetMapping("/information")
-    public User selectUser (String user_pk) {
-        return userService.selectUser(user_pk);
-    }
-    //마이페이지 회원 정보 수정
-    @PostMapping("/update")
-    public void updateUser (User user) {
-        userService.updateUser(user);
-    }
-    //마이페이지 예매 목록 조회
-    @GetMapping("/bookinglist")
-    public Map<String, Object> selectBooking (String user_pk) {
-        return userService.selectBooking(user_pk);
-    }
-    //마이페이지 무비로그 조회
-    @GetMapping("/movielog")
-    public Map<String, Object> selectMovieLog (String user_pk) {
-        return userService.selectMovieLog(user_pk);
-    }
-    //마이페이지 포인트 조회
-    @GetMapping("/point")
-    public int selectPoint (String user_pk) {
-        return userService.selectPoint(user_pk);
-    }
-    //마이페이지 포인트 적립/소멸 조회
-    @GetMapping("/pointdetail")
-    public Map<String, Object> selectPointDetail (String user_pk) {
-        return userService.selectPointDetail(user_pk);
     }
 }
--- src/main/java/data/service/user/UserService.java
+++ src/main/java/data/service/user/UserService.java
@@ -75,28 +75,4 @@ public class UserService {
     public int selectFindPass (Map<String, String> map) {
         return userRepository.selectFindPass(map);
     }
-    //마이페이지 유저 정보 출력
-    public User selectUser (String user_pk) {
-        return userRepository.selectUser(user_pk);
-    }
-    //마이페이지 회원 정보 수정
-    public void updateUser (User user) {
-        userRepository.updateUser(user);
-    }
-    //마이페이지 예매 목록 조회
-    public Map<String, Object> selectBooking (String user_pk) {
-        return userRepository.selectBooking(user_pk);
-    }
-    //마이페이지 무비로그 조회
-    public Map<String, Object> selectMovieLog (String user_pk) {
-        return userRepository.selectMovieLog(user_pk);
-    }
-    //마이페이지 포인트 조회
-    public int selectPoint (String user_pk) {
-        return userRepository.selectPoint(user_pk);
-    }
-    //마이페이지 포인트 적립/소멸 조회
-    public Map<String, Object> selectPointDetail (String user_pk) {
-        return userRepository.selectPointDetail(user_pk);
-    }
 }
