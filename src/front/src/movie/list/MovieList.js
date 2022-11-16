918461e06ebcbe29b69017a6e7c6ff2b6bf92398
greetjeehun <greet123@naver.com>
2022-11-16 20:04:28 +0900

    [Feat] 영화리스트 데이터 출력 완료, 영화 관람등급, 좋아요 수(위시리스트 추가 요망)

--- src/front/src/movie/list/MovieList.js
+++ src/front/src/movie/list/MovieList.js
@@ -1,70 +1,71 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./MovieList.css"
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import MovieCard from "../MovieCard";
+import {Button, Divider, ScopedCssBaseline} from "@mui/material";
+import {FavoriteBorderOutlined} from "@material-ui/icons";
 
 function MovieList(props) {
     const navi = useNavigate();
 
+    const [mlist, setMlist] = useState([]);
 
-    const [mlist, setMlist] = useState([
-        {
-            movie_num:1,
-            text:"조커"
-        },
-        {
+    const ListUrl = localStorage.url + "/movie/selectMovieList?";
 
-            movie_num:2,
-            text:"어벤져스"
-        },
-        {
-            movie_num:3,
-            text:"토르"
-        },
-        {
-            movie_num:4,
-            text:"헐크"
-        },
-        {
-            movie_num:5,
-            text:"아이언맨"
-        },
-        {
-            movie_num:6,
-            text:"타노스"
-        },
-        {
-            movie_num:7,
-            text:"그루트"
-        },
-        {
-            movie_num:8,
-            text:"스파이더맨"
-        }
-    ]);
-
-
-
-
-
+    const getData =()=>{
+        axios.get(ListUrl)
+            .then((res)=>{
+                setMlist(res.data);
+                console.log(res.data);
+            });
+    }
+    //페이지 로딩시 데이터 가져오기
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <div>
             <div className={'mlistall'} >
-                <button type={'button'} className={'mlist1'}>예매율</button>
-                <button type={'button'} className={'mlist2'}>평점순</button>
+                <button type={'button'} className={'reserveBtn'}>예매율</button>
+                <button type={'button'} className={'starBtn'}>평점순</button>
             </div>
             <h1 style={{textAlign:'center', marginTop:'100px', marginBottom:'50px'}}>영화리스트</h1>
             <div className={'mplist'}>
-                {mlist.map((list) => (
-                    <div className={'onebt'}>
-                        <div className={'mp1'} onClick={() => navi(`/movie/detail/${list.movie_num}`)}>{list.text}</div>
-                        <button className={'snbt'} type={'button'} onClick={() => navi("/ticketing")}>예매하기</button>
+                {mlist.map((item,i) => (
+                    <div className={"movie-list-items"} key={i}>
+                        <MovieCard movie_data={item}/>
+                        <div className={"movie-card-text"}>
+                            <div className={"tit-area"}>
+                                <ScopedCssBaseline/>
+                                <span className={"movie-grade"}>12</span>
+                                <span className={"tit"}>{item.m_name}</span>
+                            </div>
+                            <div className={"rate-date"}>
+                                <span className={"rate"}>예매율 : {item.reserve_rate}%</span>
+                                <span className={"date"}>개봉일 : {item.m_edate}</span>
+                            </div>
+                            <div className={"btn-div"}>
+                                <span className={"like-btn"}>
+                                    <Button variant="outlined" startIcon={<FavoriteBorderOutlined />}>
+                                    좋아요
+                                </Button>
+                                </span>
+                                <span className={"book-btn"}>
+                                <Button
+                                    variant={"contained"}
+                                    sx={{
+                                        width:"120px",
+                                        marginLeft:"10px"
+                                    }}
+                                    onClick={() => navi("/ticketing")}>예매</Button>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 ))}
-
-
             </div>
-
         </div>
     );
 }
