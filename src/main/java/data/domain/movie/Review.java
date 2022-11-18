b707a3a57fcb78b0c3089540f4d1a1535ba7c570
J-LOOK-J <y000945@naver.com>
2022-11-18 12:09:15 +0900

    [Refactor] revw_date 칼럼 추가

--- src/main/java/data/domain/movie/Review.java
+++ src/main/java/data/domain/movie/Review.java
@@ -1,7 +1,10 @@
 package data.domain.movie;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.apache.ibatis.type.Alias;
+
+import java.sql.Timestamp;
 
 @Setter
 @Getter
@@ -15,5 +18,7 @@ public class Review {
     private int user_pk;
     private float revw_star;
     private String revw_text;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private Timestamp revw_date;
 
 }
