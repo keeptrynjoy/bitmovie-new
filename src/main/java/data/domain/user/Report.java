bd163e68c3ce068a95b348d5a1ce45312496fb05
J-LOOK-J <y000945@naver.com>
2022-11-18 11:43:35 +0900

    [Refactor] 코드수정

--- src/main/java/data/domain/user/Report.java
+++ src/main/java/data/domain/user/Report.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 @Getter
 @Setter
-@Builder(builderMethodName = "reportBuilder")
-@NoArgsConstructor
-@AllArgsConstructor
 public class Report {
     private int review_pk;
     private int user_pk;
