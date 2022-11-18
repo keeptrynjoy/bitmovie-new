ae82a1aab516833d4d465819fa1f165a31cbdc93
keeptrynjoy <sungmin4218@gmail.com>
2022-11-18 10:34:05 +0900

    [FIX] 김성민 1.ObjectMapper로 값 파싱할때 날짜 변환되는 사항 수정중

--- src/main/java/com/example/bitmovie/BitmovieApplication.java
+++ src/main/java/com/example/bitmovie/BitmovieApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @ComponentScan({"data.*"})
 @MapperScan({"data.*"})
