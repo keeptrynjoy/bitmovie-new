c7891d1770bc31bf1d706f87b1e4432fbc07ca34
keeptrynjoy <sungmin4218@gmail.com>
2022-11-21 18:31:01 +0900

    [Feat] 김성민 1. booking 상영시간 로직 작성중

--- src/main/java/data/controller/pay/BookingController.java
+++ src/main/java/data/controller/pay/BookingController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,21 @@ public class BookingController {
         List<Movie> screeningMovies = bookingService.selectScreeningMovieList();
 
         return new ResponseEntity<>(screeningMovies,HttpStatus.OK);
+    }
+
+    /* 상영시간 고유키에 해당하는 예매된 자석리스트 조회*/
+    @GetMapping("/reserved_seat")
+    public ResponseEntity<?> reservedSeatList(@RequestParam int screentime){
+
+        String reserved_seat = bookingService.reservedSeatList(screentime); ;
+        try {
+            List<String> list = Arrays.asList(reserved_seat.split(","));
+            list.sort(Comparator.naturalOrder());
+
+            return new ResponseEntity<>(list,HttpStatus.OK);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            return new ResponseEntity<>("요청한 상영시간이 존재하지 않습니다.",HttpStatus.BAD_REQUEST);
+        }
     }
 }
