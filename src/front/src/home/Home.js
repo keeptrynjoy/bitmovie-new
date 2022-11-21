f8e79eaaf36f8066fce90567e31069ab5f3db4ec
greetjeehun <greet123@naver.com>
2022-11-21 18:12:08 +0900

    [Feat] 영화 디테일 등장인물추가, 메인페이지 영화 트레일러 추가

--- src/front/src/home/Home.js
+++ src/front/src/home/Home.js
@@ -4,82 +4,66 @@ import Rank from "./Rank";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Autoplay, Navigation, Pagination} from "swiper";
 import ReactPlayer from "react-player";
+import axios from "axios";
+import {CircularProgress} from "@mui/material";
 
 function Home(props) {
     localStorage.url=process.env.REACT_APP_URL;
 
     const [loading, setLoading] = useState(true);
+    const [video_loading, setVideo_loading] = useState(true);
     const [movies, setMovies] = useState([]);
-    const [reviews, setReviews] = useState([
-        {
-            name : "404랑해"
-        },
-        {
-            name : "4조!"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅 :)"
-        },
-        {
-            name : "파이팅22 :)"
-        },
+    const [selected_movie,setSelected_movie] = useState("");
+    const [selected_movie_data,setSelected_movie_data] = useState([]);
+    const [reviews, setReviews] = useState([]);
 
-    ]);
-
-    //kobis key
-    const key ='3e56c5d518bc82f65d4d1d16806fdd37';
+    // kobis 영화진흥원 key
+    // const key ='3e56c5d518bc82f65d4d1d16806fdd37';
+    // 더 무비 key
+    const key = "7a447c04dbde1f8464230be65ef469eb";
     const today = new Date();
     const targetDT = today.getFullYear()+(today.getMonth()).toString().padStart(2,0)+(today.getDate().toString().padStart(2,0));
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDT}`
-            )
-        ).json();
-        setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-        setLoading(false);
+    const getMovies = async (type) => {
+        // kobis 영화진흥원 박스 오피스 api
+        // const json = await (
+        //     await fetch(
+        //         `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDT}`
+        //     )
+        // ).json();
+        // setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+        // setLoading(false);
+
+        //더 무비 api
+        await axios.get(`https://api.themoviedb.org/3/movie/${type}?api_key=${key}&language=ko&page=1&region=kr`)
+            .then((res)=>{
+                console.log(res.data);
+                setMovies(res.data.results);
+                setSelected_movie(res.data.results[0].id);
+            })
     };
 
-    let carousels = document.querySelectorAll('.glide');
+    //페이지 로딩시 데이터 가져오기
     useEffect(() => {
-        getMovies();
+        getMovies("popular").then(r=>{
+            setLoading(false);
+        });
     }, []);
 
-    const movieChart = async()=>{
+    const movieChart = () =>{
         setLoading(true);
-        const json = await (
-            await fetch(
-                `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDT}`
-            )
-        ).json();
-        setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-        setLoading(false);
+        getMovies("popular").then(r=>{
+            setLoading(false);
+        });
     }
 
-    const bitChart=()=>{
+    const bitChart= () =>{
         setLoading(true);
-        setMovies([]);
-        setLoading(false);
+        getMovies("upcoming").then(r=>{
+            setLoading(false);
+        });
     }
 
+    let carousels = document.querySelectorAll('.glide');
     useEffect(() => {
         for(let i = 0 ; i < carousels.length; i++){
             let glide = new Glide(carousels[i], {
@@ -91,56 +75,81 @@ function Home(props) {
         }
     }, [carousels]);
 
+    const selectMovie=async ()=>{
+        const getMovieUrl = `${localStorage.url}/movie/selectMovieData?movie_pk=${selected_movie}`;
+        await axios.get(getMovieUrl)
+            .then((res)=>{
+                console.log(res.data);
+                setSelected_movie_data(res.data.data);
+            })
+    }
+
+    useEffect(()=>{
+        setVideo_loading(true);
+        selectMovie().then(r=>{
+            setVideo_loading(false);
+        });
+    },[selected_movie]);
+
+    //xUDhdCsLkjU
+
     return (
         <div style={{textAlign:'center'}}>
             <div className="main-slider" >
                 <h1 style={{textAlign:'center', marginBottom:'30px'}}>영화 예고편</h1>
-                <ReactPlayer
-                    url={process.env.PUBLIC_URL + 'https://www.youtube.com/watch?v=xUDhdCsLkjU'}
-                    width='100%'
-                    height='400px'
-                    playing={true}
-                    muted={true}
-                    controls={true}
-                    loop={true}
-
-                />
+                {
+                    video_loading ?
+                        (<CircularProgress/>)
+                        :
+                        <ReactPlayer
+                            url={process.env.PUBLIC_URL + `https://www.youtube.com/watch?v=${selected_movie_data.m_video}`}
+                            width='100%'
+                            height='400px'
+                            playing={true}
+                            muted={true}
+                            controls={true}
+                            loop={true}
+                        />
+                }
             </div>
             <div>
                 <button type={"button"} className={'bt1'} onClick={movieChart}>무비차트</button>&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button type={"button"} className={'bt2'} onClick={bitChart}>예매순위</button>
+                <button type={"button"} className={'bt2'} onClick={bitChart}>상영예정작</button>
             </div>
             <br/>
-            <div className="Home">
-                {
-                    loading ?
-                        (<p>loading...</p>)
-                        :
-                        (
-                            <div>
-                                <div className="frames glide__track" data-glide-el="track">
-                                    <Swiper className="myswiper"
-                                            modules={[Navigation, Pagination, Autoplay]}
-                                            pagination={{ clickable: true }}
-                                            navigation
-                                            effect
-                                            speed={800}
-                                            loop={true}
-                                            slidesPerView={5}
-                                            autoplay={{delay: 2000,
-                                            disableOnInteraction:false}}
-                                    >
-                                        {movies.map((movie,i) => (
-                                            <SwiperSlide style={{width:'1000px'}} key={i}>
-                                                <Rank movie={movie}/>
-                                            </SwiperSlide>
-                                        ))}
-                                    </Swiper>
+            <div className={"wtf"}>
+                <div className="Home">
+                    {
+                        loading ?
+                            (<CircularProgress/>)
+                            :
+                            (
+                                <div>
+                                    <div className="frames glide__track" data-glide-el="track">
+                                        <Swiper className="myswiper"
+                                                modules={[Navigation, Pagination, Autoplay]}
+                                                pagination={{ clickable: true }}
+                                                navigation
+                                                effect
+                                                speed={800}
+                                                loop={true}
+                                                slidesPerView={5}
+                                                autoplay={{delay: 5000,
+                                                    disableOnInteraction:false}}
+                                        >
+                                            {movies && movies.map((movie,i) => (
+                                                <SwiperSlide style={{width:'1000px'}} key={i}>
+                                                    <Rank movie={movie} key={i} idx={i}
+                                                          selected_movie={selected_movie} setSelected_movie={setSelected_movie}/>
+                                                </SwiperSlide>
+                                            ))}
+                                        </Swiper>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                }
+                            )
+                    }
+                </div>
             </div>
             <h1 style={{textAlign:'center',marginTop:'100px'}}>Movie Review</h1>
             <div className={"testt"}>
@@ -154,4 +163,5 @@ function Home(props) {
         </div>
     )
 }
+
 export default Home;
