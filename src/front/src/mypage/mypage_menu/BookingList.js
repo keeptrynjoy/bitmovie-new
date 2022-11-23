a605145419279319710fae3df989973c7d180709
greetjeehun <greet123@naver.com>
2022-11-23 17:00:23 +0900

    [Feat] 예매내역 결제 취소 버튼 추가, 무비 로그 css 조금 수정

--- src/front/src/mypage/mypage_menu/BookingList.js
+++ src/front/src/mypage/mypage_menu/BookingList.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {Pagination} from "@mui/material";
 import usePagination from "../../service/UsePagination";
+import moment from "moment";
 
 function BookingList(props) {
     const list=props.booking_list;
     let [page, setPage] = useState(1);
-    const PER_PAGE = 10;
+    const PER_PAGE = 3;
 
     const count = Math.ceil(list.length / PER_PAGE);
     const _DATA = usePagination(list, PER_PAGE);
@@ -15,34 +16,83 @@ function BookingList(props) {
         _DATA.jump(p);
     };
 
+    const test=(e)=>{
+        console.log(list);
+    }
+
     return (
         <div>
-            <div className={"mypage-contents-title"}>
-                결제 내역
+            <div className={"mypage-contents-title"} onClick={test}>
+                예매 내역
             </div>
-            <table className={"mypage-table"}>
-                <thead>
-                <tr>
-                    <th>결제번호</th>
-                    <th>상영관</th>
-                    <th>결제액</th>
-                    <th>결제일</th>
-                </tr>
-                </thead>
-                <tbody>
+            {/*<table className={"mypage-table"}>*/}
+            {/*    <thead>*/}
+            {/*    <tr>*/}
+            {/*        <th>예약번호</th>*/}
+            {/*        <th>예매일</th>*/}
+            {/*        <th>상품명</th>*/}
+            {/*        <th>장소</th>*/}
+            {/*        <th>이용일</th>*/}
+            {/*        <th>현재 상태</th>*/}
+            {/*    </tr>*/}
+            {/*    </thead>*/}
+            {/*    <tbody>*/}
+            {/*    {*/}
+            {/*        list && _DATA.currentData().map((item,i)=>(*/}
+            {/*                <tr key={i}>*/}
+            {/*                    <td>{item.booknumber}</td>*/}
+            {/*                    <td>{moment(item.issue).format("YYYY-MM-DD")}</td>*/}
+            {/*                    <td>{item.title}</td>*/}
+            {/*                    <td>{item.theater} {item.screen}</td>*/}
+            {/*                    /!*<td>{moment(item.date).format("YYYY-MM-DD")}{moment(item.)}</td>*!/*/}
+            {/*                </tr>*/}
+            {/*            )*/}
+            {/*        )*/}
+            {/*    }*/}
+            {/*    </tbody>*/}
+            {/*</table>*/}
+            <ul className={"booking-list"}>
                 {
                     list && _DATA.currentData().map((item,i)=>(
-                            <tr key={i}>
-                                <td>{item.booknumber}</td>
-                                <td>{item.theater}{item.screen}</td>
-                                <td>{item.price}원</td>
-                                <td>{item.issue}</td>
-                            </tr>
+                            <li key={i}>
+                                <div className={"booking-info"}>
+                                    <span style={{width:"100%"}}>예매 번호: {item.booknumber}</span>
+                                    <div className={"box-image"}>
+                                        <span className={"thumb-image"}>
+                                            <img alt={""} src={`https://image.tmdb.org/t/p/w500${item.poster.split(",")[0]}`}/>
+                                        </span>
+                                    </div>
+                                    <div className={"box-contents"}>
+                                        <div className={"movie-title"}>
+                                            <div className={"movie-title-kr"}>{item.title}</div>
+                                        </div>
+                                        <div className={"contents-info"}>
+                                            <div>
+                                                관람 극장 : {item.theater} &nbsp; {item.screen}
+                                            </div>
+                                            <div>
+                                                관람 인원 : 성인: {item.adult}명, 청소년: {item.youth}명
+                                            </div>
+                                            <div>
+                                                관람 일시: {moment(item.date).format("YYYY-MM-DD")}&nbsp;&nbsp;{item.begin.substring(0,5)}
+                                            </div>
+                                            <div>
+                                                관람 좌석: 좌석
+                                            </div>
+                                            <div>
+                                                예매일 : {moment(item.issue).format("YYYY-MM-DD")}
+                                            </div>
+                                            <div>
+                                                결제액 : {item.price}원
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
                         )
                     )
                 }
-                </tbody>
-            </table>
+            </ul>
             <div className={"table-pagination"}>
                 <Pagination
                     count={count}
--- src/front/src/mypage/mypage_menu/MovieLog.js
+++ src/front/src/mypage/mypage_menu/MovieLog.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import moment from "moment/moment";
+import usePagination from "../../service/UsePagination";
+import {Pagination} from "@mui/material";
 
 function MovieLog(props) {
     const list = props.movie_log;
+    let [page, setPage] = useState(1);
+    const PER_PAGE = 3;
+
+    const count = Math.ceil(list.length / PER_PAGE);
+    const _DATA = usePagination(list, PER_PAGE);
+
+    const handleChange = (e, p) => {
+        setPage(p);
+        _DATA.jump(p);
+    };
+
     return (
         <div className={"movie-log"}>
             <div className={"mypage-contents-title"}>
@@ -10,7 +23,7 @@ function MovieLog(props) {
             </div>
             <ul className={"movie-log-list"}>
                 {
-                    list && list.map((item,i)=>(
+                    list && _DATA.currentData().map((item,i)=>(
                             <li key={i}>
                                 <div className={"article-movie-info"}>
                                     <div className={"box-image"}>
@@ -21,7 +34,7 @@ function MovieLog(props) {
                                     <div className={"box-contents"}>
                                         <div className={"movie-title"}>
                                             <div className={"movie-title-kr"}>{item.title}</div>
-                                            <p>{item.engtitle}</p>
+                                            <div className={"movie-title-en"}><p>{item.engtitle}</p></div>
                                         </div>
                                         <div className={"movie-date"}>
                                             {moment(item.date).format("YYYY-MM-DD")} {item.begin.substring(0, item.begin.length - 3)} ~ {item.endtime.substring(0, item.endtime.length - 3)}
@@ -34,12 +47,19 @@ function MovieLog(props) {
                                         </div>
                                     </div>
                                 </div>
-
                             </li>
                         )
                     )
                 }
             </ul>
+            <div className={"table-pagination"}>
+                <Pagination
+                    count={count}
+                    size="large"
+                    page={page}
+                    onChange={handleChange}
+                />
+            </div>
         </div>
     );
 }
