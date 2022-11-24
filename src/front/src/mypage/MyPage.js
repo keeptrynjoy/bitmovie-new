69327c636243ad7d70210d5699deefe32a2e7897
greetjeehun <greet123@naver.com>
2022-11-24 11:36:57 +0900

    [Feat] 헤헿gpgp

--- src/front/src/mypage/MyPage.js
+++ src/front/src/mypage/MyPage.js
@@ -61,7 +61,7 @@ function MyPage(props) {
         <div className={"mypage-div"}>
             <div className={"upper-info-div"}>
                 <div className={"photo-div"}>
-                    <img alt={"프로필 사진"} src={photo} className={"profile-photo"}/>
+                    {/*<img alt={"프로필 사진"} src={} className={"profile-photo"}/>*/}
                     <input type={"file"} hidden={true} id={"profile-photo"} onChange={onUploadImage}/>
                     <div className={"photo-fix-icon"} onClick={triggerCLick}>
                         <EditIcon/>
