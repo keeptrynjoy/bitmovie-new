e04e360bb995fef6e787ede2792cee3e85cf5631
taemin <myloopmy@gmail.com>
2022-11-28 15:54:41 +0900

    [TEST] 무비리스트 css fix

--- src/front/src/ticketing/booking/MovieList.js
+++ src/front/src/ticketing/booking/MovieList.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './MovieList.css'
 import axios from "axios";
 import Age from "../../service/Age";
+import {CircularProgress} from "@mui/material";
 
 
 
@@ -13,17 +14,37 @@ const MovieList = (props) => {
 
     const {input,setInput,changeData}=props;
     const [mvlist,setMvlist] = useState([]);
-
+    const [loading,setLoading]=useState(true);
 
 
     const get=()=>{
-        axios.get('http://localhost:8282/booking/screening_list')
+        setLoading(true);
+        axios.get('http://localhost:8282/booking/screening_list?BorA=after')
             .then((response) =>{
                 setMvlist(response.data);
+                setLoading(false);
+            });
+    }
+
+    const get2=()=>{
+        setLoading(true);
+        axios.get('http://localhost:8282/booking/screening_list?order_stand=reserve_rate&BorA=after')
+            .then((response) =>{
+                setMvlist(response.data);
+                setLoading(false);
 
             });
     }
 
+    const get3=()=>{
+        setLoading(true);
+        axios.get('http://localhost:8282/booking/screening_list?order_stand=revw_avgstar&BorA=after')
+            .then((response) =>{
+                setMvlist(response.data);
+                setLoading(false);
+
+            });
+    }
 
 
     useEffect(()=>{
@@ -41,21 +62,33 @@ const MovieList = (props) => {
         <div>
 
             <div className={'mvcolor'}>
-                <p style={{paddingLeft:'20px', paddingTop:'10px',fontSize:'20px'}}>영화</p>
-                {mvlist.map((list,i)=>(
-                    <ul key={i} style={{paddingLeft:'15px', marginBottom:'5px'}}>
-                        <li style={{listStyle:'none' ,width:'400px' ,wordBreak: 'break-all'}} >
+                <p style={{ paddingTop:'10px',fontSize:'20px', textAlign:'center'}}>영화</p>
+               <button className={'mvbtn1'} onClick={get} style={{marginLeft:'25px', backgroundColor:'white', fontSize:'15px',marginBottom:'0px', color:'gray'}}>이름순</button><button className={'mvbtn1'} onClick={get2} style={{marginLeft:'20px', backgroundColor:'white', fontSize:'15px',marginBottom:'0px'}}><p style={{marginBottom:'0', fontSize:'15px', color:'gray'}}>예매율순</p></button><button className={'mvbtn1'} onClick={get3} style={{marginLeft:'20px', backgroundColor:'white', fontSize:'15px',marginBottom:'0px', color:'gray'}}>평점순</button>
+                <hr/>
+                {
+                    loading ?
+                        <div style={{display:"flex",justifyContent:"center",alignItems:'center', height:'400px'}}>
+                            <CircularProgress/>
+                        </div>
+                        :
 
-                                {/*<img src={age} style={{width:'30px', float:'left'}}/>*/}
 
-                            <button className={'mvbtn'} key={i} style={{fontSize:'15px'}} onClick={changeData}  value={JSON.stringify(list)} name={'movie'}>
-                                <Age age={list.m_age_grd} size={20}/>&nbsp;
-                        <div style={{width:'200px'}} className={'txt'} >&nbsp;{list.m_name}</div>
-                    </button>
+                            mvlist.map((list, i) => (
+                                <ul key={i} style={{paddingLeft: '15px', marginBottom: '5px'}}>
+                                    <li style={{listStyle: 'none', width: '400px', wordBreak: 'break-all'}}>
 
-                        </li>
-                    </ul>
-                ))}
+                                        {/*<img src={age} style={{width:'30px', float:'left'}}/>*/}
+
+                                        <button className={'mvbtn'} key={i} style={{fontSize: '15px'}}
+                                                onClick={changeData} value={JSON.stringify(list)} name={'movie'}>
+                                            <Age age={list.m_age_grd} size={20}/>&nbsp;
+                                            <div style={{width: '200px'}} className={'txt'}>&nbsp;{list.m_name}</div>
+                                        </button>
+
+                                    </li>
+                                </ul>
+                            ))
+                        }
             </div>
 
         </div>
