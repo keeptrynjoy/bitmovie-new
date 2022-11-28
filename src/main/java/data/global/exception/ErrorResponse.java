5bf83067c976a439bb56da935ac3b132e8f4493a
keeptrynjoy <sungmin4218@gmail.com>
2022-11-28 14:49:43 +0900

    [Refactor] 김성민 - global exception 활성화

--- src/main/java/data/global/exception/ErrorResponse.java
+++ src/main/java/data/global/exception/ErrorResponse.java
@@ -1,19 +1,17 @@
 package data.global.exception;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+@Getter
+@EqualsAndHashCode
 public class ErrorResponse {
     private final HttpStatus status;
     private final String message;
     private final List<String> errors;
-
-    public ErrorResponse(HttpStatus status, String message, List<String> errors) {
-        this.status = status;
-        this.message = message;
-        this.errors = errors;
-    }
 
     public ErrorResponse(HttpStatus status, String message, String error) {
         this.status = status;
@@ -27,4 +25,10 @@ public class ErrorResponse {
                 errorCode.name(),
                 errorCode.getDetail());
     }
+
+    //    public ErrorResponse(HttpStatus status, String message, List<String> errors) {
+//        this.status = status;
+//        this.message = message;
+//        this.errors = errors;
+//    }
 }
