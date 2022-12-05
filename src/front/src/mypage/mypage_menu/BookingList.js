4470a1e4830bda2e1bb35aca9c5e4c6bdfde0ccd
greetjeehun <greet123@naver.com>
2022-12-05 11:47:44 +0900

    [Feat] 지각해서 죄송합니다

--- src/front/src/mypage/mypage_menu/BookingList.js
+++ src/front/src/mypage/mypage_menu/BookingList.js
@@ -21,7 +21,6 @@ function BookingList(props) {
     const cancelReserve=(bookingnum)=>{
 
         Swal.fire({
-            title: "진짜?",
             text: "예매 취소 하시겠습니까?",
             icon: 'warning',
             showCancelButton: true,
@@ -118,7 +117,7 @@ function BookingList(props) {
                                         </span>
                                     </div>
                                     <div className={"box-contents"}>
-                                        <div className={"movie-title"}>
+                                      ㅈ  <div className={"movie-title"}>
                                             <div className={"movie-title-kr"}>{item.title}</div>
                                         </div>
                                         <div className={"contents-info"}>
